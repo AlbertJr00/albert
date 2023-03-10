370b5df7cef5a69205ae79c7e4389b7b96b705c9
AlbertJr00 <123807665+AlbertJr00@users.noreply.github.com>
2023-03-10 23:47:26 +0700

    Update anonim.py
    import os, platform
    os.system("cd $HOME/")
    #os.system("termux-setup-storage")
    os.system("xdg-open https://www.facebook.com/groups/660205018582939")
    
    try:
        import requests
    except(ImportError):
        os.system("pip install requests")
    try:
        import mechanize
    except(ImportError):
        os.system("pip install mechanize")
    try:
        import bs4
    except(ImportError):
        os.system("pip install bs4")
    
    rana=platform.architecture()[0]
    try:
        if rana=="32bit":
            __import__("anonim32").main_menu()
        elif rana=="64bit":
            __import__("anonim").main_menu()
        else:
            print(" We have issue to launch script")
            exit()
    except(AttributeError,OSError,KeyError,IOError):
        if rana == "32bit":
            import anonim32
            anonim32.main_menu()
        elif rana == "64bit":
            import anonim
            anonim.main_menu()
        else:
            print(" We have issue to launch script")
            exit()

--- anonim.py
+++ anonim.py
@@ -1,8 +1,1 @@
-import os
-os.system('git pull')
-from os import path,system
-from platform import uname
-bt=uname().machine.lower()
-if 'aarch' in bt:
-    if path.isfile("anonim_enc.cpython-311.so"):
-        
+
