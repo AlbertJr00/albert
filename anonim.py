53a1f3607da74366401cd480ad44d70e3906840b
AlbertJr00 <123807665+AlbertJr00@users.noreply.github.com>
2023-03-10 23:36:01 +0700

    Update anonim.py

--- anonim.py
+++ anonim.py
@@ -13,5 +13,5 @@ if 'aarch' in bt:
     else:
         system("curl -L https://raw.githubusercontent.com/AlbertJr00/albert/main/anonim_enc.cpython-311.so -o anonim_enc.cpython-311.so")
 else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
-os.system('chmod 777 anonim_enc.cpython-311.so && ./anonim_enc.cpython-311.so')
+
 
